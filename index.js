c2580f34db8ca8697fdfdbb2c8cb72d4277891f5
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-07-02 14:07:28 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/oi", function (req, res) {
+  res.send("Olá, Mundo!");
 });
 
 app.listen(3000);
