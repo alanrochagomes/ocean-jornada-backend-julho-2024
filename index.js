f544c87bffcceb584efa35c3ad4a633420dd462c
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-07-03 13:47:13 +0000

    Endpoint Create `[POST] /item` adicionado (WIP)

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const app = express();
-
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
-
+// Desafio: criar endpoint /oi que exibe "Olá, mundo!"
 app.get("/oi", function (req, res) {
-  res.send("Olá, Mundo!");
+  res.send("Olá, mundo!");
 });
-
 // Lista de Personagens
 const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"];
-
 // Read All - [GET] /item
 app.get("/item", function (req, res) {
   // Pegamos a lista e enviamos como resposta HTTP
   res.send(lista);
+});
+
+// Sinalizamos para o Express que vamos usar JSON no Body
+app.use(express.json());
+
+// Create - [POST] /item
+app.post("/item", function (req, res) {
+  console.log(req.body);
+  res.send("Create");
 });
 
 app.listen(3000);
